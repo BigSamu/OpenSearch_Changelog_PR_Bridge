16f17dba6fd55dd39c6a221a4d00bc511f2b460d
Johnathon Bowers <johnathonbowers@gmail.com>
2024-01-18 19:26:18 -0500

    Simplify internal and external error messages to one error message
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- src/errors/base.errors.js
+++ src/errors/base.errors.js
@@ -1,18 +1,16 @@
 /**
- * A base custom error class with default internal and public messages that can be overridden.
+ * A base custom error class with a default error message that can be overridden.
  */
 export class CustomError extends Error {
     /** 
      * Creates a new CustomError instance.
      * @param {number} status - The HTTP status code.
-     * @param {string} internalMessage - The internal error message to be logged for server-side debugging.
-     * @param {string} publicMessage - The public error message to be returned in the response.
+     * @param {string} message - The error message to be returned in the response.
      */
-    constructor(status, internalMessage = "An internal error occurred", publicMessage = "Internal Server Error") {
-        super(internalMessage);
+    constructor(status, message = "Internal Server Error") {
+        super(message);
         this.status = status;
-        this.internalMessage = internalMessage;
-        this.publicMessage = publicMessage;
+        this.message = message;
         this.name = this.constructor.name;
     }
 }
