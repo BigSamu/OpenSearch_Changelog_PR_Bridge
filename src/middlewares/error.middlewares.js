e8a2ab2d772eeb2aebd47167c6de7072680ace70
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-01-19 13:34:19 -0300

    modifiying console.log errorRequestHandler() function
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src/middlewares/error.middlewares.js
+++ src/middlewares/error.middlewares.js
@@ -1,10 +1,9 @@
 export const errorRequestHandler = (err, req, res, next) => {
-  console.error(err.stack);
 
   // Set the response status code and message
   const statusCode =  err.status || 500;
   const message = err.message || "Internal Server Error";
-
+  console.error(`Error in request to OpenSearch Changelog PR Bridge service: ${message}`);
   res.status(statusCode).json({
     error: {
       status: statusCode,
