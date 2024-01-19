badb195f0842e534d2c32541caf04290639fd5e5
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2024-01-19 17:15:28 -0300

    delete errors folder and update auth.middlewares.js
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,6 +1,5 @@
 import { authServices } from "../services/index.js"; // Adjust the path as necessary
 import { CHANGELOG_PR_BRIDGE_API_KEY } from "../config/constants.js";
-import { UnauthorizedAPIKeyError } from "../errors/index.js";
 
 export async function ensureGitHubAppInstalled(req, res, next) {
   const { owner, repo } = req.query;
@@ -39,7 +38,7 @@ export const verifyReceivedApiKey = (req, res, next) => {
   const receivedApiKey = req.headers['x-api-key'];
   const authorizedApiKey = CHANGELOG_PR_BRIDGE_API_KEY;
   if (!receivedApiKey || receivedApiKey !== authorizedApiKey) {
-    return res.status(403).json({
+    return res.status(401).json({
       error: {
         status: 401,
         message: `Incorrect API Key. Access unauthorized.`,
