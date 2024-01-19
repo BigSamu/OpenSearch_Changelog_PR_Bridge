da1781a17abf78adcc06e3cfc93f06b29a5a701e
Johnathon Bowers <johnathonbowers@gmail.com>
2024-01-19 09:25:40 -0500

    Update errorRequestHandler middleware function
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- src/middlewares/error.middlewares.js
+++ src/middlewares/error.middlewares.js
@@ -1,10 +1,9 @@
 export const errorRequestHandler = (err, req, res, next) => {
-  // Log internal message for server-side debugging
-  console.error(err.internalMessage || err.stack);
+  console.error(err.stack);
 
   // Set the response status code and message
   const statusCode =  err.status || 500;
-  const message = err.publicMessage || err.message || "Internal Server Error";
+  const message = err.message || "Internal Server Error";
 
   res.status(statusCode).json({
     error: {
